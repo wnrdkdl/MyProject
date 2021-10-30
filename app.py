2cabadde3b6eb62203c5b3c30579d38c25a99cb3
wnrdkdl <wnrdkdl@naver.com>
2021-10-30 17:04:51 +0900

    Project version 2 completed (Product search)

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
